03b776f8bb09f57633cb8ca46474335295b9dac6
Kelly Sciandra <kellysciandradev@icloud.com>
2019-08-21 06:40:52 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,27 +4,29 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    # all.limit(5)
+    Boat.limit(5).to_a
   end
 
   def self.dinghy
+    Boat.where('length < 20')
     # where("length < 20")
   end
 
   def self.ship
+    Boat.where('length >= 20')
     # where("length >= 20")
   end
 
   def self.last_three_alphabetically
-    # all.order(name: :desc).limit(3)
+    Boat.all.order(name: :desc).limit(3)
   end
 
   def self.without_a_captain
-    # where(captain_id: nil)
+    Boat.where(captain_id: nil)
   end
 
   def self.sailboats
-    # includes(:classifications).where(classifications: { name: 'Sailboat' })
+    Boat.includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
   def self.with_three_classifications
@@ -32,14 +34,14 @@ class Boat < ActiveRecord::Base
     # regularly. Just know that we can get this out of the database in
     # milliseconds whereas it would take whole seconds for Ruby to do the same.
     #
-    # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    Boat.joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
   def self.non_sailboats
-    # where("id NOT IN (?)", self.sailboats.pluck(:id))
+    Boat.where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
 
   def self.longest
-    # order('length DESC').first
+    Boat.order('length DESC').first
   end
 end
